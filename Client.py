b16f263405dd8ac02ffba8b13cd093b7da887908
redder558 <brianton@tonthat.com>
2018-01-26 16:36:03 -0800

    Revert "Added Better Username Stuff"
    This reverts commit 3125ccc0737862732be90634b1611ee0c7409c56.

--- Client.py
+++ Client.py
@@ -6,12 +6,10 @@ except ImportError:
 from ScrolledText import ScrolledText
 import socket
 import threading
-from time import strftime, sleep
+from time import strftime,sleep
 import Queue
 import os
-
-
-def recv_thread(conn, q):
+def recv_thread(conn,q):
     while True:
         try:
             data = conn.recv(4096)
@@ -20,55 +18,46 @@ def recv_thread(conn, q):
         except socket.error:
             q.put('Server Disconnect')
 
-
 def send_msg(txtbx):
-    global s, e
+    global s,e
     if len(e.get()) == 0:
-        showwarning("Warning", "Can't Send Empty Message")
+        showwarning("Warning","Can't Send Empty Message")
         return None
     else:
         s.send(e.get())
         txtbx.config(state=NORMAL)
-        txtbx.insert(END, '\n' + '[' + strftime('%m/%d/%Y %I:%M:%S') + ']~ ME: ' + e.get())
+        txtbx.insert(END, '\n' + '['+strftime('%m/%d/%Y %I:%M:%S')+']~ ME: '+ e.get())
         txtbx.config(state=DISABLED)
-        txtbx.see(END)
-        e.delete(0, END)
-
-
+        e.delete(0,END)
 def update_from_queue():
-    global schedule_queue, txtbx
+    global schedule_queue,txtbx
     try:
         while True:
             line = schedule_queue.get_nowait()
             if line != 'Server Disconnect':
                 txtbx.config(state=NORMAL)
-                txtbx.insert(END, '\n' + line)
+                txtbx.insert(END,'\n'+line)
                 txtbx.config(state=DISABLED)
-                txtbx.see('end')
             else:
-                showerror('Server', 'Server Has Disconnected')
+                showerror('Server','Server Has Disconnected')
                 os._exit(0)
     except Queue.Empty:
         pass
-    root.after(100, update_from_queue)
-
-
+    root.after(100,update_from_queue)
 def GUI(window):
-    global e, s, schedule_queue, txtbx
+    global e,s,schedule_queue,txtbx
     txtbx = ScrolledText(window, state=DISABLED)
     txtbx.grid(row=0)
     txtbx.see(END)
     e = Entry(window)
     e.grid(row=1, column=0)
-    butt = Button(window, text='Send', command=lambda: send_msg(txtbx))
-    window.bind('<Return>', lambda event: send_msg(txtbx))
+    butt = Button(window, text='Send', command=lambda:send_msg(txtbx))
+    window.bind('<Return>',lambda event: send_msg(txtbx))
     butt.grid(row=1, column=1)
     schedule_queue = Queue.Queue()
     update_from_queue()
     sleep(.3)
-    threading.Thread(target=lambda: recv_thread(s, schedule_queue)).start()
-
-
+    threading.Thread(target=lambda: recv_thread(s,schedule_queue)).start()
 if __name__ == '__main__':
     ip_addr = raw_input('IP Address: ')
     username = raw_input('Username: ')
