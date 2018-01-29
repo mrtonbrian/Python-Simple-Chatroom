99d3065cccd5394030af0162583db32f5f53d479
redder558 <brianton@tonthat.com>
2018-01-28 18:58:16 -0800

    Register Prompting / Compression
    - Added User Registration
    - Added zlib Compression On Text W/ Lengths > 170

--- Client.py
+++ Client.py
@@ -10,6 +10,28 @@ import threading
 from time import strftime, sleep
 import Queue
 import os
+import hashlib
+import zlib
+
+
+# https://stackoverflow.com/a/29126154/8935887
+class register_login_popup:
+    def __init__(self, root):
+        self.choice = ''
+        self.master = Toplevel(root)
+        master = self.master
+        Label(master, text='Register Or Login?').grid(row=0, column=1)
+        Button(master, text='Register', command=lambda: self.set_choice('r')).grid(row=1, column=0)
+        Button(master, text='Login', command=lambda: self.set_choice('l')).grid(row=1, column=2)
+
+    def set_choice(self, c):
+        self.master.destroy()
+        self.choice = c
+
+    def show(self):
+        self.master.wm_deiconify()
+        self.master.wait_window()
+        return self.choice
 
 
 def recv_thread(conn, q):
@@ -17,7 +39,10 @@ def recv_thread(conn, q):
         try:
             data = conn.recv(8192)
             if data:
-                q.put(data)
+                try:
+                    q.put(zlib.decompress(data))
+                except:
+                    q.put(data)
         except socket.error, e:
             print repr(e)
             q.put('Server Disconnect')
@@ -29,7 +54,10 @@ def send_msg(txtbx):
         showwarning("Warning", "Can't Send Empty Message")
         return None
     else:
-        s.send(e.get())
+        txt = e.get()
+        if len(txt) >= 170:
+            txt = zlib.compress(txt, 9)
+        s.send(txt)
         txtbx.config(state=NORMAL)
         txtbx.insert(END, '\n' + '[' + strftime('%m/%d/%Y %I:%M:%S') + ']~ ME: ' + e.get())
         txtbx.config(state=DISABLED)
@@ -54,38 +82,17 @@ def update_from_queue():
         pass
     root.after(100, update_from_queue)
 
+
 def check_ip(ip):
     try:
         socket.inet_aton(ip)
         return True
     except:
         return False
+
+
 def GUI(window):
     global e, s, schedule_queue, txtbx
-    textbox_frame = Frame(window,width=70)
-    textbox_frame.grid(row=0)
-    window.columnconfigure(1,weight=1)
-    txtbx = ScrolledText(textbox_frame)
-    txtbx.see(END)
-    txtbx.pack(fill=BOTH)
-
-    bottom_row = Frame(window,width=70,height=20)
-    e = Entry(bottom_row,width=70)
-    e.pack(fill='both',expand=1)
-
-    butt = Button(window, text='Send', command=lambda: send_msg(txtbx))
-    window.bind('<Return>', lambda event: send_msg(txtbx))
-    butt.grid(column=1)
-    schedule_queue = Queue.Queue()
-    update_from_queue()
-    bottom_row.grid(row=1)
-
-    threading.Thread(target=lambda: recv_thread(s, schedule_queue)).start()
-    e.focus_set()
-if __name__ == '__main__':
-    root = Tk()
-    port = 9001
-    global s
     ip_addr_verify = True
     while True:
         root.focus_set()
@@ -111,9 +118,89 @@ if __name__ == '__main__':
             else:
                 ip_addr_verify = 'INVALID'
     root.focus_set()
-    username = askstring('Username', 'Enter Your Desired Username')
+    user_login_selection = get_login_request(root)
+    s.send(user_login_selection)
+    if user_login_selection == 'r':
+        times = 1
+        while True:
+            if times == 1:
+                username = askstring('Username', 'Enter Your Desired Username')
+            else:
+                username = askstring('Username', 'Username Taken \nEnter Another Username')
+            s.send(username)
+            code = s.recv(2048)
+            if code == 'VALID':
+                while True:
+                    password = password_input(root, 'Enter Your Desired Password').show()
+                    break
+                s.send(hashlib.sha256(password).hexdigest())
+                break
+            else:
+                times += 1
+    textbox_frame = Frame(window, width=70)
+    textbox_frame.grid(row=0)
+    window.columnconfigure(1, weight=1)
+    txtbx = ScrolledText(textbox_frame)
+    txtbx.see(END)
+    txtbx.pack(fill=BOTH)
+
+    bottom_row = Frame(window, width=70, height=20)
+    e = Entry(bottom_row, width=70)
+    e.pack(fill='both', expand=1)
+
+    butt = Button(window, text='Send', command=lambda: send_msg(txtbx))
+    window.bind('<Return>', lambda event: send_msg(txtbx))
+    butt.grid(column=1)
+    schedule_queue = Queue.Queue()
+    update_from_queue()
+    bottom_row.grid(row=1)
+
+    threading.Thread(target=lambda: recv_thread(s, schedule_queue)).start()
+    e.focus_set()
+
+
+class password_input:
+    def __init__(self, root, t):
+        self.final_pswd = ''
+        self.master = Toplevel(root)
+        master = self.master
+        Label(master, text=t).grid(row=0)
+        Label(master, text='Password: ').grid(row=1, column=0)
+        self.pswd = Entry(master, show='*')
+        self.pswd.grid(row=1, column=1)
+        Label(master, text='Reenter Your Password: ').grid(row=2, column=0)
+        self.reentered = Entry(master, show='*')
+        self.reentered.grid(row=2, column=1)
+        Button(master, text='OK', command=self.on_ok).grid(row=3)
+        master.bind('<Return>', lambda event: self.on_ok)
+
+    def on_ok(self):
+        inp_pswd = self.pswd.get()
+        reentered_pswd = self.reentered.get()
+        if inp_pswd != '' and reentered_pswd != '':
+            if inp_pswd == reentered_pswd:
+                self.final_pswd = self.pswd.get()
+                self.master.destroy()
+            else:
+                showerror('Password', "Passwords Don't Match")
+        else:
+            showerror('Error', "You Didn't Enter Anything")
+
+    def show(self):
+        self.master.wm_deiconify()
+        self.master.wait_window()
+        return self.final_pswd
+
+
+def get_login_request(root):
+    result = register_login_popup(root).show()
+    return result
+
+
+if __name__ == '__main__':
+    root = Tk()
+    port = 9001
     GUI(root)
-    s.send(username)
     root.mainloop()
     s.close()
     os._exit(0)
--- Server.py
+++ Server.py
@@ -4,7 +4,9 @@ from time import strftime, sleep
 import sys
 import netifaces
 import atexit
-
+import random
+import hashlib
+import zlib
 
 def get_ip():
     if sys.platform != 'win64' and sys.platform != 'win32':
@@ -18,32 +20,39 @@ def get_ip():
     else:
         return socket.gethostbyname(socket.getfqdn())
 
+
 def parse_transcript(username):
-    with open('transcript.txt','r') as f:
+    with open('transcript.txt', 'r') as f:
         lines = f.readlines()
     new_lines = []
     for i in lines:
         if 'Startup' in i:
             continue
         if username in i and ('SERVER' not in i):
-            new_lines.append(i.replace(username,'ME'))
+            new_lines.append(i.replace(username, 'ME'))
         else:
             new_lines.append(i)
-    #print lines
+    # print lines
     return new_lines
 
 
 def client(conn, u):
     global conns
-    #conn.send('Welcome To The Chatroom, You Can Now Send / Receive Messages')
+    # conn.send('Welcome To The Chatroom, You Can Now Send / Receive Messages')
     try:
+        full = ''
         for i in parse_transcript(u):
-            conn.send(i.strip('\n')+'\n')
+            full += i.strip('\n') + '\n'
+        compressed = zlib.compress(full,9)
+        conn.send(compressed)
     except:
-        remover(conn,conn.getpeername())
+        remover(conn, conn.getpeername())
     sleep(.5)
     f = addr_to_user.values()
-    del f[f.index(u)]
+    try:
+        del f[f.index(u)]
+    except:
+        pass
     f = ', '.join(f)
     try:
         if len(f) != 0:
@@ -51,11 +60,15 @@ def client(conn, u):
         else:
             conn.send('No One Else Is Currently Online')
     except:
-        remover(conn,conn.getpeername())
+        remover(conn, conn.getpeername())
     try:
         while True:
             data = conn.recv(8192)
             if data:
+                try:
+                    data = zlib.decompress(data)
+                except:
+                    pass
                 msg = '[' + strftime('%m/%d/%Y %I:%M:%S') + ']~ ' + u + ': ' + data
                 send_all(msg, conn)
                 print msg
@@ -84,11 +97,52 @@ def send_all(msg, conn):
     for i in conns:
         try:
             if i != conn:
-                i.send(msg)
+                if len(msg) <= 170:
+                    i.send(msg)
+                else:
+                    i.send(zlib.compress(msg,9))
             else:
                 pass
         except:
             remover(conn, addr_to_user[conn.getpeername()])
+
+
+def generate_salt(char_amount=32, suitable_chars="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"):
+    final = ""
+    r = random.SystemRandom()
+    for i in xrange(0, char_amount):
+        final += r.choice(suitable_chars)
+    return final
+
+
+def handle_conn(conn, addr):
+    login_or_username = conn.recv(1024)
+    if login_or_username == 'r':
+        usernames = []
+        try:
+            with open('usernames.dat', 'r') as f:
+                for i in f.readlines():
+                    usernames.append(i.strip('\n'))
+        except:
+            usernames = []
+        while True:
+            username = conn.recv(1024)
+            for i in usernames:
+                splitted = i.split('`')
+                if splitted[0] == username:
+                    conn.send('EXISTING')
+            else:
+                conn.send('VALID')
+                password = conn.recv(4096)
+                break
+        with open('usernames.dat','a+') as f:
+            salt = generate_salt()
+            f.write(username+'`'+hashlib.sha256(password+salt).hexdigest()+'`'+salt+'\n')
+    print '[' + strftime('%m/%d/%Y %I:%M:%S') + ']~' + ' Connection ' + addr[0] + ' ---> ' + username
+    send_all('[' + strftime('%m/%d/%Y %I:%M:%S') + ']~ ' + 'SERVER: ' + username + ' Has Connected', conns)
+    addr_to_user[addr] = username
+    conns.append(conn)
+    threading._start_new_thread(client, (conn, username))
 
 
 if __name__ == '__main__':
@@ -100,14 +154,9 @@ if __name__ == '__main__':
     print get_ip()
     s.bind((get_ip(), 9001))
     s.listen(500)
-    global conns
+    global conns, addr_to_user
     conns = []
     addr_to_user = {}
     while True:
         conn, addr = s.accept()
-        username = conn.recv(1024)
-        print '[' + strftime('%m/%d/%Y %I:%M:%S') + ']~' + ' Connection ' + addr[0] + ' ---> ' + username
-        send_all('[' + strftime('%m/%d/%Y %I:%M:%S') + ']~ ' + 'SERVER: ' + username + ' Has Connected', conns)
-        addr_to_user[addr] = username
-        conns.append(conn)
-        threading._start_new_thread(client, (conn, username))
+        threading._start_new_thread(handle_conn, (conn, addr))
